107980622f145bb73564ba7583a6e92b677ee9f6
Ironbrain <mfuxic@gmail.com>
2019-03-27 11:42:35 +0900

    正規表現から開始

---
+++ labo/phplabo2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+  <head>
+    <meta charset="UTF-8">
+    <title>php2</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+
+  <body>
+  <h1>PHP第二部だよッ！！！！！</h1>
+  </body>
+</html>
