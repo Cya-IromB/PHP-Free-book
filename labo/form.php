d8ba7353b5a91a4df190f42c5ebd97520a4a5ec5
Ironbrain <mfuxic@gmail.com>
2019-04-02 02:18:53 +0900

    formについて色々ググったり。裏でスニペット作ってた

---
+++ labo/form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+  <head>
+    <meta charset="UTF-8">
+    <title>forms</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+
+  <body>
+    <form action="form.php" method="post">
+      名前: <br>
+      <input type="text" name="userName" size="30" value="" placeholder=""><br />
+      メールアドレス: <br>
+      <input type="text" name="mail" size="30" value="" placeholder=""><br />
+      コメントをどうぞ！: <br>
+      <textarea name="comment"  cols="30" rows="10"></textarea><br />
+      <br />
+      <input type="submit" value="押せええええええええええ！！！" />
+    </form>
+
+    
+
+  </body>
+</html>
