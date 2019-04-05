b29ddb9f81ee3234f8f154067f5871d4f3c3d57f
Ironbrain <mfuxic@gmail.com>
2019-04-06 00:59:12 +0900

    $_POST,$_REQUEST,$_GETの挙動が分かった！

--- labo/form.php
+++ labo/form.php
@@ -19,7 +19,47 @@
       <input type="submit" value="押せええええええええええ！！！" />
     </form>
 
-    
+    <p>上のフォームは連想配列$_POSTへ格納される。</p>
+    <p>上記のケースの場合、連想配列には……</p>
+
+    <pre><code>
+    $_POST['userName'] => (名前:で入力した文字列);
+    $_POST['mail'] => (メールアドレスで入力した文字列);
+    $_POST[comment] => (コメントをどうぞ！ で入力された文字列);
+
+    $_REQUEST['userName'] => (名前:で入力した文字列);
+    $_REQUEST['mail'] => (メールアドレスで入力した文字列);
+    $_REQUEST[comment] => (コメントをどうぞ！ で入力された文字列);
+    </code></pre>
+
+    <br>
+
+    <div class="phpOutput">
+    <?php
+    if(!empty($_POST)){
+      echo '<p>POST======================================</p>';
+      foreach ($_POST as $key => $value) {
+        echo "${key} -> ".htmlspecialchars($value).'<br />';
+      }
+    }
+
+    if(!empty($_REQUEST)){
+      echo '<p>REQUEST===================================</p>';
+      foreach ($_REQUEST as $key => $value) {
+        echo "${key} -> ".htmlspecialchars($value).'<br />';
+      }
+    }
+
+    if($_GET){
+      echo '<p>GET=======================================</p>';
+      foreach ($_GET as $key => $value) {
+        echo "${key} -> ".htmlspecialchars($value).'<br />';
+      }
+    }
+     ?>
+     </div>
+
+
 
   </body>
 </html>
